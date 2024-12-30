40e7d1c9a71a5b846b12b1d424d1a6accdb1e150
Ali Tavakoli <118619813+A-tavv@users.noreply.github.com>
2024-12-30 16:33:22 +0100

    Update libraries.py

--- Source_code/libraries.py
+++ Source_code/libraries.py
@@ -1,1 +1,8 @@
+#### **In this task**:
+We used a dataset called MWD with four weather classes: 'cloudy', 'rain', 'shine', and 'sunrise' (represented as 0, 1, 2, and 3).
+We applied three different models:
+1. DenseNet 201v2
+2. VGG16
+3. ResNet 101v2
 
+Each model was trained for 10 epochs. After training, we compared the results. also we plot training accuracy, loss, and confusion matrix for each model.
